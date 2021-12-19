dd251e6e16d5ff26c7f1dd03dc55e5079982c027
sumit <sumit.jha420@gmail.com>
2021-12-19 15:28:42 +0530

    commiting code

---
+++ sort_arr_three_number.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // coode here 
+           map<int,int> mp;
+   for(int i = 0 ; i < n ; i++)
+   {
+       mp[a[i]]++;
+   }
+   
+   int start = 0;
+   for(auto item : mp)
+   {
+       while(item.second-- > 0)
+       {
+           a[start++] = item.first;
+       }
+   }
+    }
+    
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
